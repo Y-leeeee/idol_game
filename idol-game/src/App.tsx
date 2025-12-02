dd6b81bd7e3541a6e1aa9fad61d0a239027afad6
Y-leeeee <lyr890802@gmail.com>
2025-12-02 12:49:40 +0100

    Add stat grouping and day counter feature

--- idol-game/src/App.tsx
+++ idol-game/src/App.tsx
@@ -1,35 +1,62 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [singing, setSinging] = useState(0);
+  const [dancing, setDancing] = useState(0);
+  const [visuals, setVisuals] = useState(0);
+  const [idolName, setIdolName] = useState("");
+  const [day, setDay] = useState(1);
+
+  function handleDayPass() {
+    setDay(day + 1);
+    const rand = Math.floor(Math.random() * 3);
+
+    if (rand === 0 && singing < 100) {
+      setSinging(singing + 1);
+    } else if (rand === 1 && dancing < 100) {
+      setDancing(dancing + 1);
+    } else if (rand === 2 && visuals < 100) {
+      setVisuals(visuals + 1);
+    }
+  }
 
   return (
     <>
+      <h1>Create your own KPOP idol!</h1>
+      <label>
+        Idol name :{" "}
+        <input
+          value={idolName}
+          onChange={(event) => setIdolName(event.target.value)}
+        />
+      </label>
+      <p>{idolName ? `${idolName}'s stats` : "Stats"}</p>
+      <p>Day : {day}</p>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <h3>Singing</h3>
+        <p>{singing}</p>
+        <button onClick={() => singing < 100 && setSinging(singing + 1)}>
+          +
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <h3>Dancing</h3>
+        <p>{dancing}</p>
+        <button onClick={() => dancing < 100 && setDancing(dancing + 1)}>
+          +
+        </button>
+      </div>
+      <div>
+        <h3>Visuals</h3>
+        <p>{visuals}</p>
+        <button onClick={() => visuals < 100 && setVisuals(visuals + 1)}>
+          +
+        </button>
+      </div>
+      <button onClick={handleDayPass}>Spend a day(random skill +1)</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
